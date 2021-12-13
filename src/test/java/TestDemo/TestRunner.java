0763b6794920b7aea7611e98cdb5f4e589b3fdc2
LDAP\tthoa3 <tthoa3@cmc.com.vn>
2021-12-13 22:58:31 +0700

    commit Demo test java

---
+++ src/test/java/TestDemo/TestRunner.java
@@ -0,0 +1,12 @@
+package TestDemo;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.
+import cucumber.api.junit.Cucumber;
+
+public class TestRunner {
+
+
+    @RunWith(Cucumber.class)
+    public class runTest {}
+}
